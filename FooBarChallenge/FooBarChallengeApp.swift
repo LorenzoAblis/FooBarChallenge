1aebceb275af92e0862add5ec39acd5f9ec65189
LorenzoAblis <lablis228609@gmail.com>
2023-09-01 09:58:35 -0500

    Initial Commit

---
+++ FooBarChallenge/FooBarChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FooBarChallengeApp.swift
+//  FooBarChallenge
+//
+//  Created by Lorenzo J. Ablis on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FooBarChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
